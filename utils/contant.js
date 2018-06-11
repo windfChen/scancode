ce67b7f28da2c0b3fff9ca9c84e055e9085c2593
windfChen <q3400p@sina.cn>
2018-06-11 23:20:36 +0800

    小样式修改

--- utils/contant.js
+++ utils/contant.js
@@ -1,8 +1,12 @@
 
 const shareInfo = content => {
+  let path = '/pages/code/code'
+  if (content) {
+    path = `/pages/scan/scan?content=${content}`
+  }
   return {
     title: '二维码小工具',
-    path: `/pages/index/index${content ?'?content=' + content: ''}`,
+    path: path,
     imageUrl: '/images/defaultQrcode.png'
   }
 }
